6d2c0a428470b422db2d4ac8bcc9c702bb0243ac
unknown <hatim.web.dev@gmail.com>
2025-10-01 09:17:24 +0200

    maj

--- src/main/java/com/ecom/testcart/TestCartApplication.java
+++ src/main/java/com/ecom/testcart/TestCartApplication.java
@@ -1,14 +1,13 @@
 package com.ecom.testcart;
 
-import com.ecom.testcart.config.RsakeysConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableConfigurationProperties(RsakeysConfig.class)
+
 public class TestCartApplication {
 
 	public static void main(String[] args) {
