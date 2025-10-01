3a2783ca5f1d20d7bb593301b87b40ddb919aac1
unknown <hatim.web.dev@gmail.com>
2025-10-01 11:08:45 +0200

    maj

--- src/main/java/com/ecom/testcart/config/RsakeysConfig.java
+++
@@ -1,9 +0,0 @@
-package com.ecom.testcart.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import java.security.interfaces.RSAPublicKey;
-
-@ConfigurationProperties(prefix = "rsa")
-public record RsakeysConfig(RSAPublicKey publicKey) {
-}
